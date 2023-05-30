fa53a9d0ea14996b58023ca40bc1a66f46a50752
Samuel <samuelromero079@gmail.com>
2023-05-30 17:14:40 -0400

    completed day 1

--- day 1/helloWorld.py
+++ day 1/helloWorld.py
@@ -1,12 +1,9 @@
 # Output & Input: print, input
 
 # print Hello World!
-<<<<<<< HEAD
+
+# print Hello {user}! using input
 print("Hello World!") 
 # print Hello {user}! using input
 samuel = input("samuel romero")
 print("hello"+samuel) 
-=======
-
-# print Hello {user}! using input
->>>>>>> debca71 (updated for day 2)
