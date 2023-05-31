7f29d9fb5584c8966076e601dde23dd2926f18d4
Lisa Nam <lsyn2016@gmail.com>
2023-05-31 11:12:54 -0400

    operator fix & update gitignore

--- day 1/if_else.py
+++ day 1/if_else.py
@@ -6,10 +6,26 @@
 if True:
     print("It's True")
 
+<<<<<<< HEAD
 # &(and)
 
 # or
 
+=======
+if False:
+    print("It's False")
+num = 1
+print("Num is", num)
+if num == 0:
+    print("It's 0!")
+if num == 1:
+    print("It's 1!")
+
+# and
+
+# or
+
+>>>>>>> 45253c8 (operator fix & update gitignore)
 # not
 
 # comparision == !=
---
+++ day 1/nums.py
@@ -0,0 +1,22 @@
+#Primitive Data Types - int & float, operations, bulit-in functions - type(), int(), float(), str()
+
+#print int
+
+#print float
+
+#add/sub + -
+
+#multiply * & divide /
+
+#exponentiation **
+
+#modulus %
+
+#floor division //
+
+#type()
+
+#type conversion - int(), float(), str()
+
+
+
